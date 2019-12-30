cc52e6b9f34068ebc6e1a9ad62d12906ea7e0cb8
Trần Phước Đức <tranphuocduc1998@gmail.com>
2019-12-30 20:48:36 +0700

    make it better

--- api/models/connectPg.js
+++ api/models/connectPg.js
@@ -2,10 +2,10 @@
 const { Pool, Client } = require('pg');
 // const connectionString = 'postgres://qyammapipejnvg:054a921bddaf23e3157c25f83caaae011144fe4dfd5b1d9a59d932bc9c1b25cc@ec2-107-22-197-30.compute-1.amazonaws.com:5432/d7jhvlh7d63qqg'
 const client = new Client({
-    user: 'qyammapipejnvg',
-    host: 'ec2-107-22-197-30.compute-1.amazonaws.com',
-    database: 'd7jhvlh7d63qqg',
-    password: '054a921bddaf23e3157c25f83caaae011144fe4dfd5b1d9a59d932bc9c1b25cc',
+    user: process.env.userpg,
+    host: process.env.host,
+    database: process.env.database,
+    password: process.env.password,
     port: 5432,
     ssl: true,
 });
